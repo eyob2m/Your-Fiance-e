ad28bbd466284ee74ce27dfeabfc3f2f7cf064dd
eyob2m <eyob2etemariam@gamil.com>
2023-12-18 04:25:41 +0300

    initial commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import o from './photo_2023-12-18_04-17-43.jpg'
 export default function Footer() {
   return (
     <div className='w-full flex  text-4xl font-bold text-gray-500 justify-center items-center stiky bottom-0 me h-[20vh]'>
     
 <p className='bg-white p-4'>Footer</p>
-
+<div> <img className='absolute ybel h-[200px] top-2' src={o} alt=""/></div>
     </div>
   )
 }
